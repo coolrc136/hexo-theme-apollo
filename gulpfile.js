ed80bf17cab501d44154c8bc1212a8b43b7d82d2
coolrc136 <qq1782536964@live.com>
2019-03-03 14:54:47 +0800

    :hankey: chore(common): upgrade to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass-china');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 // 一次性编译 Sass 
-gulp.task('sass', function() {
+gulp.task('sass',function() {
     return gulp.src('./source/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
 });
 
 // 实时编译
-gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
-});
+gulp.task('default', gulp.parallel('sass',function watch() {
+    gulp.watch('./source/scss/_partial/*.scss', gulp.series('sass'));
+    gulp.watch('./source/scss/*.scss', gulp.series('sass'));
+}));
